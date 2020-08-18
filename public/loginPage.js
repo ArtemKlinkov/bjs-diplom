a49551fb8d0ee0f7a577d55e3ce8096e62efd9d0
Artem <artemklinkov@yandex.ru>
2020-08-18 22:31:53 +0300

    Страница входа и регистраци: добавлена возможность авторизации и регистрации нового пользователя

---
+++ public/loginPage.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const userForm = new UserForm();
+
+userForm.loginFormCallback = function (data) {
+    ApiConnector.login(data, (result) => {result.success ? location.reload() : this.setLoginErrorMessage(result.data)})
+};
+
+userForm.registerFormCallback = function (data) {
+    ApiConnector.register(data, (result) => {result.success ? location.reload() : this.setRegisterErrorMessage(result.data)})
+};
